3fa8626e7209416075ec3c170f61edbcb0e04a50
João Victor Ventura Martins <jvmartinscv@gmail.com>
2025-11-11 22:35:49 -0300

    Refactor global styles and update hero section
    Removed normalize.css and migrated base styles to elements.css. Added CSS variables for colors in variables.css. Updated the hero section to use Tailwind classes and new color variables for improved styling consistency.

--- src/components/pages/home/hero-section.tsx
+++ src/components/pages/home/hero-section.tsx
@@ -2,8 +2,8 @@ import { prata } from "@/components/layout/fonts"
 
 export default function HeroSection() {
     return (
-        <section className="{heroSection}">
-            <h1 className={prata.className}>SWAG</h1>
+        <section className="{heroSection} h-screen flex items-center justify-center">
+            <h1 className="text-[15rem] text-[var(--color-gray)] font-[prata] font-bold transform scale-y-50">SWAG</h1>
         </section>
     )
 }
