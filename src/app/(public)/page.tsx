53d38ba893f3a40b7b0be7afb4e6652227887eb1
João Victor Ventura Martins <jvmartinscv@gmail.com>
2025-11-11 22:51:14 -0300

    Add tickets-section a pagina
    Adiciona a secao dos tickets e suas primeiras informacoes

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/pages/home/hero-section";
+import TicketsSection from "@/components/pages/home/tickets-section";
 
 export default function Home() {
     return (
         <main>
 
             <HeroSection />
+            <TicketsSection />
 
         </main>
     )
--- src/components/pages/home/tickets-section.tsx
+++ src/components/pages/home/tickets-section.tsx
@@ -1,7 +1,26 @@
 export default function TicketsSection() {
     return (
         <section className="{ticketsSection}">
-        
+            <div className="flex items-center justify-center gap-40">
+                <div className="w-64 h-36">
+                    <p className="text-[var(--color-text)]">April 11, 2026</p>
+                    <p className="text-[var(--color-text)]">Coachella Valley Music and Arts Festival <span>2026</span></p>
+                    <p className="text-[var(--color-text)]">Indio, CA United States</p>
+                    <div className="flex gap-8 text-[var(--color-text)]">
+                        <a href="#">Tickets</a>
+                        <a href="#">RSVP</a>
+                    </div>
+                </div>
+                <div className="w-64 h-36">
+                    <p className="text-[var(--color-text)]">April 18, 2026</p>
+                    <p className="text-[var(--color-text)]">Coachella Valley Music and Arts Festival <span>2026</span></p>
+                    <p className="text-[var(--color-text)]">Indio, CA United States</p>
+                    <div className="flex gap-8 text-[var(--color-text)]">
+                        <a href="#">Tickets</a>
+                        <a href="#">RSVP</a>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
